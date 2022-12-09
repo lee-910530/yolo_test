232106144389f96adf04587593cf72ea48883dd9
lee-910530 <jacky0970711835@gmail.com>
2022-12-09 18:57:02 +0800

    hi3

---
+++ learning/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/aa/learning/src
+# Build directory: /home/aa/learning/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("example")
---
+++ learning/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/aa/learning/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/aa/learning/devel/lib:$LD_LIBRARY_PATH"
+export PWD='/home/aa/learning/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/aa/learning/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/aa/learning/src:$ROS_PACKAGE_PATH"
